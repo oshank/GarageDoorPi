54fc923f31116a5514e096c7bfc87cfe9e7d08e3
Oliver Shanklin <oliver.shanklin@outlook.com>
2016-02-12 00:57:50 -0700

    Trying to work on single file

---
+++ scripts/allTogetherNow.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+# This will be used for checking the state of the door
+
+# Pins to use GPIO25, GPIO8, GPIO7, GPIO2
+# Pins on Pi  22      24     26     3
+
+#Pin In check from switch
+pinIn = 25
+GPIO.setup(pinIn, GPIO.IN)
+#Pin out to switch
+pinOut = 8
+GPIO.setup(pinOut, GPIO.OUT)
+#Pin for Relay
+pin = 18
+GPIO.setup(pin, GPIO.OUT)
+
+def openClose(state):
+    if state == "open":
+        try:
+            time.sleep(1)
+            GPIO.output(pin, GPIO.HIGH)
+            #time.sleep(.2)
+            GPIO.output(pin, GPIO.LOW)
+            print "Closed Door"
+    elif state == "closed":
+        try:
+            time.sleep(1)
+            GPIO.output(pin, GPIO.HIGH)
+            #time.sleep(.2)
+            GPIO.output(pin, GPIO.LOW)
+            print "Opened Door"
+    GPIO.cleanup()
